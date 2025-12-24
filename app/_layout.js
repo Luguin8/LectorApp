7068d61f1690943621f28a8e48b2568b55b0ae03
Martin Lugo <martinlugo65@gmail.com>
2025-12-23 21:52:00 -0300

    modifique _layaout

--- app/_layout.js
+++ app/_layout.js
@@ -1,19 +1,25 @@
 import { Stack } from 'expo-router';
+// 1. Importamos el Provider
+import { ReaderProvider } from '../context/ReaderContext';
 
 export default function Layout() {
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#f4511e',
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-            }}
-        >
-            <Stack.Screen name="index" options={{ title: 'Biblioteca' }} />
-        </Stack>
+        // 2. Envolvemos la navegación
+        <ReaderProvider>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#f4511e',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                }}
+            >
+                <Stack.Screen name="index" options={{ title: 'Biblioteca' }} />
+                {/* Aquí la pantalla del lector recibirá configuración extra después */}
+            </Stack>
+        </ReaderProvider>
     );
 }
